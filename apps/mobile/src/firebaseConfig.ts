0be9ffa320eb6e4ed74ac13e866d542e320a7e4f
Rajiv Singh <helloiamrajiv@gmail.com>
2026-08-29 07:07:34 +0530

    fix(mobile): update firebase config to match new backend project

--- apps/mobile/src/firebaseConfig.ts
+++ apps/mobile/src/firebaseConfig.ts
@@ -6,14 +6,12 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Firebase project config
 const firebaseConfig = {
-  apiKey: "AIzaSyD6GpPOaAUbRrIyJjZnXfuSCtI7Hfwg64Y",
-  authDomain: "aarambh360-97dfe.firebaseapp.com",
-  databaseURL: "https://aarambh360-97dfe-default-rtdb.firebaseio.com",
-  projectId: "aarambh360-97dfe",
-  storageBucket: "aarambh360-97dfe.firebasestorage.app",
-  messagingSenderId: "560234410101",
-  appId: "1:560234410101:web:36f7b898768e4e58c4483c",
-  measurementId: "G-0WL0TG95LJ"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
